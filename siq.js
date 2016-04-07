7b24a592cd2a55f8bffdc5106eefdb4e10cb89ab
Jason Hamner <ke7ori@gmail.com>
2016-04-07 06:17:30 -0600

    Code from 4/5 + working crud for mongodb

--- siq.js
+++ siq.js
@@ -59,6 +59,99 @@ var db = [
 
 //REST API calls go here.
 app.get('/api/v1/entries.json', function(req, res) {
+    // find({},{content:0})
+    mongoclient.connect('mongodb://localhost:27017/test', function(err, db) {
+        if (err) {
+            throw err;
+        }db.collection('entries').find({},{subject:1}).toArray(function(err, result){
+            if (err) {
+                throw err;
+            }
+            console.log(result);
+            res.status(201).json(result);
+            db.close();
+        })
+    });
+});
+
+// IDEMPOTENT - You can repeat the operation as many times as you want without changing state.
+// Create
+app.post('/api/v1/entries.json', function(req, res){
+    console.log(req.body);
+    var subject = req.body.subject;
+    var content = req.body.content;
+    mongoclient.connect('mongodb://localhost:27017/test', function(err, db) {
+        if (err) {
+            throw err;
+        }db.collection('entries').insert(req.body, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            console.log('inserted ' + result.insertedIds[0]);
+            res.status(201).json(result.insertedIds[0]);
+            db.close();
+        });
+    });
+});
+
+// Read
+app.get('/api/v1/entries/:id.json', function(req, res){
+    // find({_id:${id}}, {content:1})
+    var id = new mongo.ObjectId(req.params.id);
+    mongoclient.connect('mongodb://localhost:27017/test', function(err, db) {
+        if (err) {
+            throw err;
+        }db.collection('entries').find({_id:id}).toArray( function(err, result) {
+            if (err) {
+                throw err;
+            }
+            console.log('db get ' + result[0]);
+            res.status(201).json(result[0]);
+            db.close()
+        });
+    });
+});
+
+// Update
+app.put('/api/v1/entries/:id.json', function(req, res){
+    // insert({})
+    var id = mongo.ObjectId(req.params.id);
+    var _subject = req.body.subject;
+    var _content = req.body.content;
+    mongoclient.connect('mongodb://localhost:27017/test', function(err, db) {
+        if (err) {
+            throw err;
+        }db.collection('entries').update({_id:id}, {subject:_subject, content:_content}, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            console.log('db update');
+            res.status(204);
+            db.close();
+        });
+    });
+});
+
+// Delete
+app.delete('/api/v1/entries/:id', function(req, res){
+    var id = mongo.ObjectId(req.params.id);
+    mongoclient.connect('mongodb://localhost:27017/test', function(err, db) {
+        if (err) {
+            throw err;
+        }db.collection('entries').remove({_id:id}, true, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            console.log('db delete ' + id);
+            res.status(204);
+            db.close();
+        });
+    });
+});
+
+/*
+//REST API calls go here.
+app.get('/api/v1/entries.json', function(req, res) {
     mongoclient.connect('mongodb://localhost:27017/test', function(err, db) {
         if (err) {
             throw err;
@@ -160,6 +253,7 @@ app.delete('/api/v1/entries/:id', function(req, res){
     console.log("server delete");
     res.sendStatus(204);
 });
+*/
 
 /*
  //REST API calls go here.
